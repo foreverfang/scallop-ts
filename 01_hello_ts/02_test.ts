605f48f5ccdcf08d45b38d6522f85faa0437c8e8
scallop <g275053739@163.com>
2021-04-12 14:45:15 +0800

    20210412 ts类型

--- 01_hello_ts/02_test.ts
+++ 01_hello_ts/02_test.ts
@@ -9,10 +9,32 @@ let b2 = true
 b2 = false
 // b = 123 // 报错
 
-function sum (a:number, b:number) : number{
-  return a+b
+function sum(a: number, b: number): number {
+    return a + b
 }
 
-console.log(sum(1,2))
+console.log(sum(1, 2))
 
-let result = sum(2,3)
+let result = sum(2, 3)
+
+// unkown 实际上就是一个类型安全的any
+// unkown 类型的变量，不能直接赋值给其他变量
+let c1: unknown
+// let c1
+// let c1: any
+c1 = 77
+c1 = 'fang'
+c1 = false
+
+let s1: string
+// s1 = c1 // 报错
+
+// 类型断言，用来告诉解析器 变量的实际类型
+s1 = c1 as string
+s1 = <string>c1
+
+function fn1(): void { }
+
+function fn2(): never {
+    throw new Error('报错了')
+}
