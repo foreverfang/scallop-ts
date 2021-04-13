2b858ed1373e481c2a5e474c79c24806cf07ad21
scallop <g275053739@163.com>
2021-04-13 16:20:07 +0800

    20210413 compilerOptions配置

--- 01_hello_ts/part2/src/index.js
+++ 01_hello_ts/part2/src/index.js
@@ -1,1 +1,4 @@
+import { hi } from './m.js';
 console.log('src/index.ts');
+var a223 = 18;
+console.log(hi);
--- 01_hello_ts/part2/src/index.ts
+++ 01_hello_ts/part2/src/index.ts
@@ -1,1 +1,6 @@
+import { hi } from './m.js'
 console.log('src/index.ts')
+let a223 = 18
+console.log(hi)
+
+// document.getElementById('id')
