8c3418383838a8953e627dcb8d8539606910d68f
scallop <g275053739@163.com>
2021-04-15 09:23:14 +0800

    20210415 tsconfig配置02

--- 01_hello_ts/part2/src/index.ts
+++ 01_hello_ts/part2/src/index.ts
@@ -21,3 +21,9 @@ function fn1(a, b) {
 function fn2() {
     // console.log(this)
 }
+
+// tsconfig.json 配置strictNullChecks为true，可以检查null的情况
+const box1 = document.getElementById("box1")
+box1?.addEventListener('click', function () {
+    console.log('click')
+})
