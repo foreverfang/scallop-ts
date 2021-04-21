d1b72f1bc821c84736e978257102d2dae69e876d
gaojianbing <g275053739@163.com>
2021-04-21 11:49:23 +0800

    20210421 写一个食物类

--- 04_snake/src/index.ts
+++ 04_snake/src/index.ts
@@ -1,3 +1,39 @@
 import './style/index.less'
 
 // 定义食物类 Food
+class Food{
+  element: HTMLElement
+
+  constructor(){
+    // "!" 表示不会为空
+    this.element = document.getElementById('food')!
+  }
+
+  /**
+   * 
+   * offsetLeft  当前元素左上角相对于  HTMLElement.offsetParent 节点的左边界偏移的像素值
+   * 设置了position属性值，只要不为static,那么offsetParent就是父元素，否则就是body
+   */
+  // 定义一个获取食物X轴坐标的方法
+  getX(){
+    return this.element.offsetLeft
+  }
+
+  // 定义一个获取食物Y轴坐标的方法
+  getY(){
+    return this.element.offsetTop
+  }
+
+  // 修改食物位置
+  change(){
+    // 生成随机位置 注意范围
+    // 食物的位置，X和Y轴偏移范围一致：最小0 最大 300 - 10 = 290
+    // 蛇移动一次就是一格，一格大小就是10，所以食物的坐标必须是10的整数倍
+
+    const left = Math.round(Math.random() * 29) * 10 
+    const top = Math.round(Math.random() * 29) * 10 
+
+    this.element.style.left = left + "px"
+    this.element.style.top = top + "px"
+  }
+}
